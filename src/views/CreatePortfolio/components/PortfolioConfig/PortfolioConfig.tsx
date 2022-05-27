9d9193a3c644ce816bd7e264dbb85e90403f1852
lddang1762 <lddang1762@csu.fullerton.edu>
2022-05-26 22:19:34 -0700

    feat(portfolio): portfolio config ui wip

--- src/views/CreatePortfolio/components/PortfolioConfig/PortfolioConfig.tsx
+++ src/views/CreatePortfolio/components/PortfolioConfig/PortfolioConfig.tsx
@@ -1,28 +1,154 @@
-import React from "react";
-import { Container, ActionIcon, Box, Text, Group, Stack, Divider, useMantineTheme, createStyles } from "@mantine/core";
-
+import React, { useState } from "react";
+import {
+  Container,
+  Group,
+  Text,
+  Stack,
+  useMantineTheme,
+  Title,
+  Tooltip,
+  UnstyledButton,
+  Loader,
+  Select,
+} from "@mantine/core";
+import { Help } from "tabler-icons-react";
 import StyledContainer from "../../../../components/layout/StyledContainer";
-import StyledInput from "../../../../components/Input/StyledInput";
+import StyledNumberInput from "../../../../components/Input/StyledNumberInput";
+import CurrencyIcon from "../../../../components/CurrencyIcon";
 
 export default function PortfolioConfig() {
   const theme = useMantineTheme();
+  const [loading, setLoading] = useState(false);
+  const precision = 18;
+
   return (
-    <StyledContainer fluid>
+    <StyledContainer size="sm">
       <Container fluid sx={{ display: "flex" }}>
-        <Stack pr="md" spacing="xl" sx={{ flexGrow: 2 }}>
-          <StyledInput
-            label="Initial Investment"
-            labelProps={{ style: { fontSize: theme.fontSizes.md } }}
-            radius="sm"
-          />
-          <StyledInput label="Asset Ratio" labelProps={{ style: { fontSize: theme.fontSizes.md } }} radius="sm" />
-          <Text>Stable Asset</Text>
-          <Divider />
-          <Text>Volatile Asset</Text>
+        <Stack pr="lg" spacing="xl">
+          <Group align="flex-end" spacing={5}>
+            <StyledNumberInput
+              label={
+                <Group spacing="sm">
+                  <Text>Take Profit Rate</Text>
+                  <Tooltip
+                    label="The amount of portfolio growth to take profit"
+                    position="right"
+                    sx={{ display: "flex" }}
+                    withArrow
+                  >
+                    <Help size={theme.fontSizes.sm} />
+                  </Tooltip>
+                </Group>
+              }
+              placeholder="2.5"
+              labelProps={{ style: { fontSize: theme.fontSizes.md } }}
+              styles={{ label: { width: 200 } }}
+              radius="sm"
+              size="md"
+              sx={{ width: 100 }}
+              min={0.1}
+              step={1}
+              precision={3}
+              hideControls
+            />
+
+            <Text color="gray" size="lg" pb={5}>
+              %
+            </Text>
+          </Group>
+          <Group spacing="sm" mb={-20}>
+            <Text>Initial Price</Text>
+            <Tooltip
+              label="Stable asset entry price for first position"
+              position="right"
+              sx={{ display: "flex" }}
+              withArrow
+            >
+              <Help size={theme.fontSizes.sm} />
+            </Tooltip>
+          </Group>
+          <Group>
+            <CurrencyIcon size={48} />
+            <Title order={4} ml={-8} mr="lg" sx={{ width: 35 }}>
+              TEST
+            </Title>
+            <StyledNumberInput
+              placeholder="Initial price"
+              radius="sm"
+              size="md"
+              sx={{ width: 275 }}
+              min={1 / Math.pow(10, precision)}
+              precision={precision}
+              rightSection={
+                <UnstyledButton color="red" onClick={() => setLoading(!loading)}>
+                  <Text color="primary">Latest</Text>
+                </UnstyledButton>
+              }
+              rightSectionWidth={70}
+              hideControls
+            />
+            {loading && <Loader size="sm" />}
+          </Group>
+          <Group spacing="sm" mb={-20}>
+            <Text>Timeframe</Text>
+            <Tooltip label="Periods needed to detect signal" position="right" sx={{ display: "flex" }} withArrow>
+              <Help size={theme.fontSizes.sm} />
+            </Tooltip>
+          </Group>
+          <Group>
+            <StyledNumberInput
+              placeholder="1"
+              radius="sm"
+              size="md"
+              sx={{ width: 150 }}
+              min={1 / Math.pow(10, precision)}
+              hideControls
+            />
+            <Select
+              data={[
+                { value: "0", label: "Hour (1H)" },
+                { value: "1", label: "Day (1D)" },
+                { value: "2", label: "Week (1W)" },
+                { value: "3", label: "Month (1M)" },
+                { value: "4", label: "Year (1Y)" },
+              ]}
+              defaultValue="0"
+              size="md"
+              sx={{ width: 150 }}
+            />
+          </Group>
+          <Group align="flex-end" spacing={5}>
+            <StyledNumberInput
+              label={
+                <Group spacing="sm">
+                  <Text>Equilibrium Band</Text>
+                  <Tooltip
+                    label="Percent change from the equilibrium price to detect signal"
+                    position="right"
+                    sx={{ display: "flex" }}
+                    withArrow
+                  >
+                    <Help size={theme.fontSizes.sm} />
+                  </Tooltip>
+                </Group>
+              }
+              placeholder="1"
+              labelProps={{ style: { fontSize: theme.fontSizes.md } }}
+              styles={{ label: { width: 200 } }}
+              radius="sm"
+              size="md"
+              sx={{ width: 100 }}
+              min={0.0001}
+              step={1}
+              precision={4}
+              hideControls
+            />
+
+            <Text color="gray" size="lg" pb={5}>
+              %
+            </Text>
+          </Group>
         </Stack>
-        <Container p="md" style={{ backgroundColor: theme.colors.paper[0], flexGrow: 1 }}>
-          asdf
-        </Container>
       </Container>
     </StyledContainer>
   );
--- src/views/CreatePortfolio/components/ReviewConfig/ReviewConfig.tsx
+++ src/views/CreatePortfolio/components/ReviewConfig/ReviewConfig.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Container, Group, Text, Stack, useMantineTheme, Title, Tooltip, Center } from "@mantine/core";
+
+import StyledContainer from "../../../../components/layout/StyledContainer";
 
 export default function ConfirmConfig() {
-  return <div>ConfirmConfig</div>;
+  return (
+    <StyledContainer size="lg">
+      <Container>
+        <Center sx={{ height: 500 }}>
+          <Text>Hello</Text>
+        </Center>
+      </Container>
+    </StyledContainer>
+  );
 }
